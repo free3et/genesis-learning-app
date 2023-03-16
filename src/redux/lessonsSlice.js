a5effd23e008c75bd061d77a712ad0146cae655a
free3et <free3et@gmail.com>
2023-03-16 12:53:05 +0200

    fix response

--- src/redux/lessonsSlice.js
+++ src/redux/lessonsSlice.js
@@ -9,16 +9,20 @@ export const lessonsSlice = createApi({
       method: "GET",
     }), */
     getAllCourses: builder.query({
-      query: (arg) =>
-        `/core/preview-courses?token=${import.meta.env.VITE_API_KEY_LESSONS}`,
+      query: (arg) => "/core/preview-courses/",
       method: "GET",
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkOTRlNjg4NS1kM2U5LTQwY2EtYTVjYy01MDRkNjZlZDVlN2QiLCJwbGF0Zm9ybSI6InN1YnNjcmlwdGlvbnMiLCJpYXQiOjE2Nzg3MDQ3NjIsImV4cCI6MTY3OTYwNDc2Mn0.Qw3LF39CDp27ZxoGzt5rikJM_OTx0eNaoyFFLxxrXUM",
+      },
     }),
     getCourse: builder.query({
-      query: (id) =>
-        `/core/preview-courses/${id}?token=${
-          import.meta.env.VITE_API_KEY_LESSONS
-        }`,
+      query: (id) => "/core/preview-courses/${id}",
       method: "GET",
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkOTRlNjg4NS1kM2U5LTQwY2EtYTVjYy01MDRkNjZlZDVlN2QiLCJwbGF0Zm9ybSI6InN1YnNjcmlwdGlvbnMiLCJpYXQiOjE2Nzg3MDQ3NjIsImV4cCI6MTY3OTYwNDc2Mn0.Qw3LF39CDp27ZxoGzt5rikJM_OTx0eNaoyFFLxxrXUM",
+      },
     }),
   }),
 });
