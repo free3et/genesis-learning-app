8396ca9e508912963268e2cecf84bda2275d05a4
free3et <free3et@gmail.com>
2023-03-16 21:25:22 +0200

    add proxy

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/genesis-learning-app/",
   plugins: [react()],
+  proxy: {
+    "/api": {
+      target:
+        "https://api.wisey.app/api/v1/core/preview-courses?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzMWZkNjYwNS1hZTkwLTQyNTYtYTZiOS05ZGFiODFkZGQyMjciLCJwbGF0Zm9ybSI6InN1YnNjcmlwdGlvbnMiLCJpYXQiOjE2Nzg4MzAzMDYsImV4cCI6MTY3OTczMDMwNn0.cAx4PZyr9puPdLDZ92A79gKkzdazyFtOYEuvvJqOaeE",
+      changeOrigin: true,
+      //secure: false,
+    },
+  },
 });
