728ae152062ed7a8b3479c0aaffb2b15557ae3bc
free3et <free3et@gmail.com>
2023-03-19 19:10:41 +0200

    fix basename

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -4,20 +4,23 @@ import { LessonsList } from "../components/LessonsList.jsx/LessonsList";
 import { Layout } from "../App";
 import { ErrorComponent } from "../components/ErrorComponent/ErrorComponent";
 
-export const router = createBrowserRouter([
-  {
-    path: "/genesis-learning-app/",
-    element: <Layout />,
-    errorElement: <ErrorComponent />,
-    children: [
-      {
-        element: <LessonsList />,
-        index: true,
-      },
-      {
-        element: <LessonPage />,
-        path: "/genesis-learning-app/:id",
-      },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <ErrorComponent />,
+      children: [
+        {
+          element: <LessonsList />,
+          index: true,
+        },
+        {
+          element: <LessonPage />,
+          path: "/:id",
+        },
+      ],
+    },
+  ],
+  { basename: "/genesis-learning-app/" }
+);
